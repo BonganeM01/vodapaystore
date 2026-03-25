df02ea1c87f46890103a7af48afba9cece946d12
bonganemsimanga <bongane.msimanga@vcontractor.co.za>
2026-03-25 10:32:06 +0200

    success attempt response attempt#10

--- api/notify.js
+++ api/notify.js
@@ -145,24 +145,24 @@ export default async function handler(req, res) {
       console.warn('[Notify] Failed to generate response signature: \n', JSON.stringify(signRes))
     }
 
-    const successResponse = await fetch('https://vodapaystore.vercel.app/api/notify', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Client-Id': clientId,
-        'Response-Time': responseTime,
-        'Signature': signature
-      },
-      body: JSON.stringify(successResponseBody)
-    })
+    // const successResponse = await fetch('https://vodapaystore.vercel.app/api/notify', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Client-Id': clientId,
+    //     'Response-Time': responseTime,
+    //     'Signature': signature
+    //   },
+    //   body: JSON.stringify(successResponseBody)
+    // })
 
-    const officialRes = await successResponse.json();
+    //const officialRes = await successResponse.json();
 
-    console.log('successResponse Full payload:\n', JSON.stringify(officialRes));
+    //console.log('successResponse Full payload:\n', JSON.stringify(officialRes));
 
     console.log('[Notify] Sending success response back to A+');
 
-    return res.status(200).json(officialRes);
+    return res.status(200).json(successResponseBody);
 
   } catch (err) {
     console.error('[Notify] Webhook error:', err);
